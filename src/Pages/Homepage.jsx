7e989fc2a095427a49530488f0ac90d22c883cec
sout233 <2273466101@qq.com>
2024-11-21 16:07:39 +0800

    ✨ feat: 2rd btn

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -60,7 +60,7 @@ function Homepage() {
           </div>
         </div>
         <div className="w-full h-screen flex md:flex-row flex-col justify-between bg-black overflow-hidden">
-          <div className="flex flex-col justify-center ml-10 h-full w-auto leading-tight z-10" ref={nulltechTitle}>
+          <div className="flex flex-col justify-center mx-10 md:ml-10 h-full w-auto leading-tight z-10" ref={nulltechTitle}>
             <h1 className="text-2xl font-bold mb-2 ~ml-1/2 big-title">无技术草履虫</h1>
             <h2
               className="text-[7rem] font-bold mb-4 leading-none ~text-6xl/8xl big-title"
@@ -70,7 +70,7 @@ function Homepage() {
             <h2 className="text-4xl font-bold ~ml-1/2 big-title">
               PARAMECIUM
             </h2>
-            <button className="btn btn-primary mt-4 w-40 ~ml-1/2">Dizzylab</button>
+            <a class="btn btn-primary mt-6 w-full md:w-56">Dizzylab <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hidden h-6 w-6 transition-transform duration-300 hover:translate-x-1 rtl:rotate-180 rtl:group-hover:-translate-x-1 md:inline-block"><path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"></path></svg></a>
           </div>
           <div className="w-[720rem] static opacity-20 xl:opacity-100">
             {/* <Spline scene="https://prod.spline.design/T2YezWy8vr3x1qE8/scene.splinecode" /> */}
