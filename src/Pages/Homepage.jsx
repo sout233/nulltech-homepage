2cd0dc24fb909a70238eeaec872593b05bb233cc
sout233 <2273466101@qq.com>
2024-11-21 09:14:20 +0800

    ✨ feat: add stagger for title

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
 import Spline from "@splinetool/react-spline";
-import { ReactLenis, useLenis } from 'lenis/react'
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { useRef } from 'react';
+import { ReactLenis, useLenis } from "lenis/react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import "./Homepage.css"
 
 function Homepage() {
   const lenis = useLenis(({ scroll }) => {
     // called every scroll
-  })
+  });
 
+  gsap.registerPlugin(useGSAP);
+
+  const nulltechTitle = useRef();
+
+  useGSAP(
+    () => {
+      gsap.from(".big-title", {
+        x: 360,
+        opacity: 0,
+        ease: "power2.inOut",
+        duration: 1,
+        stagger: 0.2,
+      });
+    },
+    { scope: nulltechTitle }
+  );
 
   return (
     <ReactLenis root>
-        <div data-theme="dark">
+      <div data-theme="dark">
         <div className="navbar fixed z-20 backdrop-blur-2xl">
           <div className="flex-1">
             <img
@@ -43,16 +60,26 @@ function Homepage() {
           </div>
         </div>
         <div className="w-full h-screen flex md:flex-row flex-col justify-between bg-black overflow-hidden">
-          <div className="flex flex-col justify-center ml-10 h-full w-auto leading-tight z-10">
-            <h1 className="text-2xl font-bold mb-2 ~ml-1/2">无技术草履虫</h1>
-            <h2 className="text-[7rem] font-bold mb-4 leading-none ~text-6xl/8xl" ref={container}>
+          <div className="flex flex-col justify-center ml-10 h-full w-auto leading-tight z-10" ref={nulltechTitle}>
+            <h1 className="text-2xl font-bold mb-2 ~ml-1/2 big-title">无技术草履虫</h1>
+            <h2
+              className="text-[7rem] font-bold mb-4 leading-none ~text-6xl/8xl big-title box"
+            >
               NULLTECH
             </h2>
-            <h2 className="text-4xl font-bold ~ml-1/2">PARAMECIUM</h2>
+            <h2 className="text-4xl font-bold ~ml-1/2 big-title">
+              PARAMECIUM
+            </h2>
           </div>
           <div className="w-[720rem] static opacity-20 xl:opacity-100">
             {/* <Spline scene="https://prod.spline.design/T2YezWy8vr3x1qE8/scene.splinecode" /> */}
-            <video autoPlay loop muted playsInline className="block absolute top-10 md:-top-6 md:right-[1rem] md:w-[50rem]">
+            <video
+              autoPlay
+              loop
+              muted
+              playsInline
+              className="block absolute top-16  md:-top-6 md:right-[1rem] md:w-[50rem]"
+            >
               <source src="cubic_c1.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
@@ -71,7 +98,7 @@ function Homepage() {
         </div>
       </div>
     </ReactLenis>
-    );
+  );
 }
 
 export default Homepage;
