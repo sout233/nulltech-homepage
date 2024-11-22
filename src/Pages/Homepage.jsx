c93b59666d30b4ff9094f0d0023c6615c0151e6a
sout_Nantang <55623894+sout233@users.noreply.github.com>
2024-11-22 12:06:20 +0800

    🐞 fix: change "class" to "className"

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -71,20 +71,20 @@ function Homepage() {
               NULLTECH
             </h2>
             <h2 className="text-4xl font-bold ~ml-1/2 big-title">PARAMECIUM</h2>
-            <div className="flex flex-col md:flex-row justify-center items-center md:justify-start md:items-start space-x-4">
-              <a className="btn btn-primary mt-6 w-full md:w-56">
-                Dizzylab{" "}
+            <div className="flex flex-col md:flex-row justify-center items-center md:justify-start md:items-start md:space-x-4">
+              <a className="btn btn-primary mt-6 w-full md:w-48">
+                Dizzylab
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="hidden h-6 w-6 transition-transform duration-300 hover:translate-x-1 rtl:rotate-180 rtl:group-hover:-translate-x-1 md:inline-block"
+                  className="hidden h-6 w-6 transition-transform duration-300 hover:translate-x-1 rtl:rotate-180 rtl:group-hover:-translate-x-1 md:inline-block"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
                   ></path>
                 </svg>
