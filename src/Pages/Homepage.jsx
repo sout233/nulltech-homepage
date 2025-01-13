43879e1ba8137c712b846a5e9b7fa7382b7fd594
sout_Nantang <55623894+sout233@users.noreply.github.com>
2025-01-13 12:40:02 +0800

    ✨ feat: 专辑一览 还有three.js

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,12 +1,37 @@
-import React, { Component } from "react";
-import Spline from "@splinetool/react-spline";
+/* eslint-disable no-unused-vars */
 import { ReactLenis, useLenis } from "lenis/react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./Homepage.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
 
 function Homepage() {
+  // eslint-disable-next-line no-unused-vars
   const lenis = useLenis(({ scroll }) => {
     // called every scroll
   });
@@ -18,6 +43,13 @@ function Homepage() {
   useGSAP(
     () => {
       gsap.from(".big-title", {
+        x: 360,
+        opacity: 0,
+        ease: "power2.inOut",
+        duration: 1,
+        stagger: 0.2,
+      });
+      gsap.from(".title-btn", {
         x: 360,
         opacity: 0,
         ease: "power2.inOut",
@@ -71,8 +103,11 @@ function Homepage() {
               NULLTECH
             </h2>
             <h2 className="text-4xl font-bold ~ml-1/2 big-title">PARAMECIUM</h2>
-            <div className="flex flex-col md:flex-row justify-center items-center md:justify-start md:items-start md:space-x-4">
-              <a className="btn btn-primary mt-6 w-full md:w-48">
+            <div className="flex flex-col md:flex-row justify-center items-center md:justify-start md:items-start md:space-x-4 title-btn">
+              <a
+                className="btn btn-primary mt-6 w-full md:w-48"
+                href="https://www.dizzylab.net/l/NullTech_PARAMECIUM/"
+              >
                 Dizzylab
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -89,11 +124,33 @@ function Homepage() {
                   ></path>
                 </svg>
               </a>
-              <a className="btn btn-neutral mt-6 w-full md:w-48" href="https://space.bilibili.com/3546388319177513">Bilibili</a>
+              <a
+                className="btn btn-neutral mt-6 w-full md:w-48"
+                href="https://space.bilibili.com/3546388319177513"
+              >
+                Bilibili
+              </a>
             </div>
           </div>
           <div className="w-[720rem] xl:w-full static opacity-20 xl:opacity-100">
             {/* <Spline scene="https://prod.spline.design/T2YezWy8vr3x1qE8/scene.splinecode" /> */}
+            {/* <Canvas>
+              <ambientLight intensity={Math.PI / 2} />
+              <spotLight
+                position={[10, 10, 10]}
+                angle={0.15}
+                penumbra={1}
+                decay={0}
+                intensity={Math.PI}
+              />
+              <pointLight
+                position={[-10, -10, -10]}
+                decay={0}
+                intensity={Math.PI}
+              />
+              <Box position={[-1.2, 0, 0]} />
+              <Box position={[1.2, 0, 0]} />
+            </Canvas> */}
             <video
               autoPlay
               loop
@@ -106,25 +163,62 @@ function Homepage() {
             </video>
           </div>
         </div>
-        
+        {/* 专辑一览 */}
         <div className="w-full h-screen flex md:flex-row flex-col justify-center items-center bg-black overflow-x-hidden">
           <div className="flex flex-col justify-center items-center text-center h-full w-auto leading-tight ">
             <h2 className="text-[7rem] font-extrabold mb-4leading-none ~text-4xl/6xl text-white">
-            浏览所有专辑
+              专辑一览
             </h2>
+            <div className="h-10"></div>
+            <div className="flex flex-row justify-center items-center space-x-10">
+              <div className=" bg-base-200 rounded-xl w-60">
+                <a href="https://www.dizzylab.net/d/NTP-001/">
+                  <img
+                    src="images/NTP-001.jpg"
+                    className="w-60 object-cover rounded-xl shadow-lg shadow-white/20 hover:transform hover:scale-105 transition-transform duration-300"
+                  ></img>
+                </a>
+                <div className="text-start p-2">
+                  <h1 className="text-xl font-bold">TECH BOOT UP!!!! 01</h1>
+                  <h2 className="text-sm">NTP-001</h2>
+                </div>
+              </div>
+              <div className="bg-base-200 rounded-xl w-60">
+                <a href="https://www.dizzylab.net/d/NTP-002/">
+                  <img
+                    src="images/NTP-002.jpg"
+                    className="w-60 object-cover rounded-xl shadow-lg shadow-white/20 hover:transform hover:scale-105 transition-transform duration-300"
+                  ></img>
+                </a>
+                <div className="text-start p-2">
+                  <h1 className="text-xl font-bold">NTP SHOWCASE</h1>
+                  <h2 className="text-sm">NTP-002</h2>
+                </div>
+              </div>
+              <div className="bg-base-200 rounded-xl w-60">
+                <a href="https://www.dizzylab.net/d/NTP-003/">
+                  <img
+                    src="images/NTP-003.jpg"
+                    className="w-60 object-cover rounded-xl shadow-lg shadow-white/20 hover:transform hover:scale-105 transition-transform duration-300"
+                  ></img>
+                </a>
+                <div className="text-start p-2">
+                  <h1 className="text-xl font-bold">PSYCHEDELiX MIND</h1>
+                  <h2 className="text-sm">NTP-003</h2>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-
         <div className="w-full h-screen flex md:flex-row flex-col justify-center items-center bg-primary overflow-x-hidden">
           <div className="flex flex-col justify-center items-center text-center h-full w-auto leading-tight ">
             <h2 className="text-[7rem] font-extrabold mb-4 leading-none ~text-6xl/8xl text-primary-content">
-            HOW WE WORK
+              HOW WE WORK
             </h2>
           </div>
         </div>
-
         <div className="w-full h-screen flex md:flex-row flex-col justify-between bg-black overflow-x-hidden">
-        <div className="flex flex-col justify-center ml-10 h-full w-auto leading-tight ">
+          <div className="flex flex-col justify-center ml-10 h-full w-auto leading-tight ">
             <h2 className="text-[7rem] font-bold mb-4 leading-none ~text-6xl/8xl">
               NULLTECH
             </h2>
